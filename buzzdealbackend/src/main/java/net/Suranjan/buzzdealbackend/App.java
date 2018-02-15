f6d879b72fd4b96be3023433bbd2f1d1a26802c6
Suranjan Dey <suranjandey2012@gmail.com>
2018-02-15 22:59:11 +0530

    Second commit

---
+++ buzzdealbackend/src/main/java/net/Suranjan/buzzdealbackend/App.java
@@ -0,0 +1,13 @@
+package net.Suranjan.buzzdealbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
